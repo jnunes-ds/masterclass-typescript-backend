73683059ec2ba0dd41305fb3e407ea2e4f4e0cba
Junior Nunes <denisonprobable13@gmail.com>
2021-05-03 14:58:33 -0300

    index.js moved to Dit directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var PORT = 3333;
-app.get('/', function (req, res) {
-    return res.send('Hello World');
-});
-app.listen(PORT);
